cf5feca0d61e2de2367e1a974014a727c21fdfba
BeiShanair <2430649818@qq.com>
2026-08-17 16:04:59 +0800

    flowerGen

--- src/main/java/com/besson/tutorial/worldgen/ModBiomeModifiers.java
+++ src/main/java/com/besson/tutorial/worldgen/ModBiomeModifiers.java
@@ -7,6 +7,7 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BiomeTags;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.levelgen.GenerationStep;
@@ -19,6 +20,10 @@ import net.neoforged.neoforge.registries.NeoForgeRegistries;
 public class ModBiomeModifiers {
     public static final ResourceKey<BiomeModifier> ADD_ICE_ETHER_TREE = registerKey("add_ice_ether_tree");
     public static final ResourceKey<BiomeModifier> ADD_SIMPLE_FLOWER = registerKey("add_simple_flower");
+    public static final ResourceKey<BiomeModifier> ADD_ICE_ETHER_ORE = registerKey("add_ice_ether_ore");
+    public static final ResourceKey<BiomeModifier> ADD_NETHER_ICE_ETHER_ORE = registerKey("add_nether_ice_ether_ore");
+    public static final ResourceKey<BiomeModifier> ADD_END_ICE_ETHER_ORE = registerKey("add_end_ice_ether_ore");
+    
     
     public static void bootstrap(BootstrapContext<BiomeModifier> context) {
         HolderGetter<PlacedFeature> placedFeature = context.lookup(Registries.PLACED_FEATURE);
@@ -33,6 +38,19 @@ public class ModBiomeModifiers {
                 biomes.getOrThrow(Tags.Biomes.IS_PLAINS),
                 HolderSet.direct(placedFeature.getOrThrow(ModPlacementFeatures.SIMPLE_FLOWER_PLACED_KEY)),
                 GenerationStep.Decoration.VEGETAL_DECORATION));
+        
+        context.register(ADD_ICE_ETHER_ORE, new BiomeModifiers.AddFeaturesBiomeModifier(
+                biomes.getOrThrow(BiomeTags.IS_OVERWORLD),
+                HolderSet.direct(placedFeature.getOrThrow(ModPlacementFeatures.ICE_ETHER_ORE_PLACED_KEY)),
+                GenerationStep.Decoration.UNDERGROUND_ORES));
+        context.register(ADD_NETHER_ICE_ETHER_ORE, new BiomeModifiers.AddFeaturesBiomeModifier(
+                biomes.getOrThrow(BiomeTags.IS_NETHER),
+                HolderSet.direct(placedFeature.getOrThrow(ModPlacementFeatures.NETHER_ICE_ETHER_ORE_PLACED_KEY)),
+                GenerationStep.Decoration.UNDERGROUND_ORES));
+        context.register(ADD_END_ICE_ETHER_ORE, new BiomeModifiers.AddFeaturesBiomeModifier(
+                biomes.getOrThrow(BiomeTags.IS_END),
+                HolderSet.direct(placedFeature.getOrThrow(ModPlacementFeatures.END_ICE_ETHER_ORE_PLACED_KEY)),
+                GenerationStep.Decoration.UNDERGROUND_ORES));
     }
     
     public static ResourceKey<BiomeModifier> registerKey(String name) {
